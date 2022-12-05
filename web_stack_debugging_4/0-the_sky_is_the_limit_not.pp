6ef83ee3ab617eac12982261f39d24388fae1ed1
Makuo67 <okekemakuo67@gmail.com>
2022-12-05 12:19:32 +0000

    fix nginx

---
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# Allow our server to handle lots of request without failing
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
